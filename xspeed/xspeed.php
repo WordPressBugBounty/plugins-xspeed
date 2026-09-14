8ea50400ae3f85d039efff8a0bb20796be295479
Sean Murphy <sean@iamseanmurphy.com>
2026-09-14 17:00:15 +0000

    1.3.1

--- xspeed/xspeed.php
+++ xspeed/xspeed.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: xSpeed Cache
  * Description: Minimal, ultra-fast caching plugin for WordPress.
- * Version: 1.3.0
+ * Version: 1.3.1
  * Requires at least: 6.0
  * Tested up to: 7.1
  * Requires PHP: 7.4
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'XSPEED_VERSION', '1.3.0' );
+define( 'XSPEED_VERSION', '1.3.1' );
 define( 'XSPEED_FILE', __FILE__ );
 define( 'XSPEED_DIR', plugin_dir_path( __FILE__ ) );
 define( 'XSPEED_URL', plugin_dir_url( __FILE__ ) );
